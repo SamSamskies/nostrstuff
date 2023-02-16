874572abd3947d4a0ea2b569ddf248b8427db98f
Sam Samskies <samsamskies@gmail.com>
2023-02-15 23:40:52 -0600

    boom nip05 samsamskies@nostrstuff.com is born

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        source: "/.well-known/nostr.json",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
