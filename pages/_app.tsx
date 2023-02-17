ab428b42f51e9032eba878abb0c539d3a5a07cbb
Sam Samskies <samsamskies@gmail.com>
2023-02-17 11:21:52 -0600

    set up vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from "next/app";
 import { TerminalContextProvider } from "react-terminal";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <TerminalContextProvider>
-      <Component {...pageProps} />
-    </TerminalContextProvider>
+    <>
+      <TerminalContextProvider>
+        <Component {...pageProps} />
+      </TerminalContextProvider>
+      <Analytics />
+    </>
   );
 }
