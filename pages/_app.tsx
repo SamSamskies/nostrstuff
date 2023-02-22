2395714e6d3100d065edfa00cd8cfab241d98326
Sam Samskies <samsamskies@gmail.com>
2023-02-22 15:29:57 -0600

    update global font styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { TerminalContextProvider } from "react-terminal";
 import { Analytics } from "@vercel/analytics/react";
+import "@/styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
