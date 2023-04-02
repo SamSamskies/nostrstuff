5f0eedb882f86ccf52f0e19b83a17db2afc384fa
Sam Samskies <samsamskies@gmail.com>
2023-04-02 06:16:43 -1000

    add tool to extract bech32 values from a nostr event (#8)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,37 @@ import type { AppProps } from "next/app";
 import { TerminalContextProvider } from "react-terminal";
 import { Analytics } from "@vercel/analytics/react";
 import "@/styles/global.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Nostr Stuff</title>
+        <meta name="description" content="Nostr stuff yo!" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <TerminalContextProvider>
         <Component {...pageProps} />
       </TerminalContextProvider>
