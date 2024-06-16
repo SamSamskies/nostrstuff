21cb0cababea19e69e4e4f31e6f83d7ce5eca379
Sam Samskies <samsamskies@gmail.com>
2024-06-16 13:53:25 -0500

    update default relays

--- constants/index.ts
+++ constants/index.ts
@@ -10,10 +10,7 @@ export const THEMES = [
 
 export const DEFAULT_RELAYS = [
   "wss://relay.damus.io",
-  "wss://relay.snort.social",
   "wss://nostr.wine",
-  "wss://eden.nostr.land",
-  "wss://nostr.fmt.wiz.biz",
-  "wss://nostr.milou.lol",
   "wss://relay.nostr.band",
+  "wss://nos.lol",
 ];
